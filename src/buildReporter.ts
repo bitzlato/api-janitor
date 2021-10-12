ad07db58ef254af3bc7f6cdebaa0444bf70a2c11
Aleksandr Diskant <velsamm@yandex.ru>
2021-10-12 14:36:25 +0300

    build reporter

--- src/buildReporter.ts
+++ src/buildReporter.ts
@@ -5,6 +5,8 @@ const janitorVersion =  require('../package.json').version
 
 const BUGSNAG_API_KEY = getConfig().bugsnag.app_key
 
-reportBuild({ apiKey: BUGSNAG_API_KEY, appVersion: janitorVersion })
+const report = { apiKey: BUGSNAG_API_KEY, appVersion: janitorVersion, releaseStage: process.env.NODE_ENV }
+
+reportBuild(report)
     .then(() => console.log('Build report sent to Bugsnag!'))
     .catch((err: { message: string }) => console.log('Fail to send build report to Bugsnag', err.message))

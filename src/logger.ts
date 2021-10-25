fc600dc4fa1ddccff4ba0a0c866e9e2ed4c8baea
Aleksandr Diskant <velsamm@yandex.ru>
2021-10-25 13:57:10 +0300

    add stacktrade to log [closes #20]

--- src/logger.ts
+++ src/logger.ts
@@ -3,13 +3,18 @@ import { LoggerLevel } from "./types";
 
 const { combine, timestamp, label, printf } = winston.format
 
-const myFormat = printf(({ level, message, label, timestamp }) => {
-    return `${timestamp} [${label}] ${level} ${message}`;
-});
+const myFormat = printf(({ level, message, label, timestamp, stack }) => {
+    if (stack) {
+        return `${timestamp} [${label}] ${level} ${message} ${stack && '\n'.concat(stack)}`
+    }
+
+    return `${timestamp} [${label}] ${level} ${message}`
+})
 
 const Logger = winston.createLogger({
     level: LoggerLevel.INFO,
     format: combine(
+        winston.format.errors({ stack: true }),
         winston.format.colorize(),
         label({ label: 'janitor' }),
         timestamp(),
@@ -17,18 +22,6 @@ const Logger = winston.createLogger({
     ),
     silent: false,
     transports: [
-        new winston.transports.File({
-            filename: 'log/logfile',
-            level: LoggerLevel.ERROR,
-        }),
-        new winston.transports.File({
-            filename: 'log/logfile',
-            level: LoggerLevel.INFO,
-        }),
-        new winston.transports.File({
-            filename: 'log/logfile',
-            level: LoggerLevel.DEBUG,
-        }),
         new winston.transports.Console()
     ],
 })
